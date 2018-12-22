b072f4c4c4e15777d078caffdd3eea74888c9e5a
MAKLs <mikeashimko@gmail.com>
2018-12-21 23:18:36 -0500

    Add module name to module file in case importing fails

--- tool_console/tool_console.psm1
+++ tool_console/tool_console.psm1
@@ -1,18 +1,19 @@
 $public = Get-ChildItem -Path (Join-Path $PSScriptRoot "public") -Recurse -Filter *.ps1
 $private = Get-ChildItem -Path (Join-Path $PSScriptRoot "private") -Recurse -Filter *.ps1
+$module = Split-Path $PSScriptRoot -Leaf
 
 #Load public functions
 foreach ($import in $public)
 {
    try {. $import.FullName}
-   catch {Write-Error ("Failed to import public function {0}" -f $import.BaseName)}
+   catch {Write-Error ("Failed to import public function {0} from module {1}" -f $import.BaseName, $module)}
 }
 
 #Load private functions
 foreach ($import in $private)
 {
    try {. $import.FullName}
-   catch {Write-Error ("Failed to import private function {0}" -f $import.BaseName)}
+   catch {Write-Error ("Failed to import private function {0} from module {1}" -f $import.BaseName, $module)}
 }
 
 #Export only public functions
